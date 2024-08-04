927639bb7d5e426cce94df56e2fcd09ca4358c58
pikalovvova1303 <pikalovvova1303@gmail.com>
2024-08-04 16:07:52 +0300

    v 0.1

--- snake-console/Dependencies.h
+++ snake-console/Dependencies.h
@@ -3,3 +3,4 @@
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
+#include <string>
--- snake-console/game.cpp
+++ snake-console/game.cpp
@@ -2,11 +2,12 @@
 
 const int CountOfSym = 109;
 
-int charPos = (CountOfSym / 2 - 5);
+int charPos = (CountOfSym / 2 - 5);							//start initializer of position of character
 int prevPos;
 
+const std::string arrMessages[10] = { "invalid input", "", "", "", "", "", "", "", "", ""}; //array of messages for unexpected situations
 
-char arrGameField[CountOfSym] =
+char arrGameField[CountOfSym] =								// main array of gamefield symbols
 {
 	'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '\n',
 	'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', '\n',
@@ -28,7 +29,7 @@ int main() {
 
 		arrGameField[charPos] = GameCharacter;
 
-		for (int i = 0; i < 109; i++) { // Loop for draw a gamefield
+		for (int i = 0; i < 109; i++) {					// Loop for draw a gamefield
 			std::cout << arrGameField[i];
 		}
 
@@ -37,22 +38,22 @@ int main() {
 		std::cin >> action;
 		
 
-		if (action == 'w') {
-			charPos = charPos - 11;
-			
-		}
-		else if (action == 's') {
-			charPos = charPos + 11;
-		}
-		else if (action == 'a') {
-			charPos = charPos - 1;
-		}
-		else if (action == 'd') {
-			charPos = charPos + 1;
-		}
-		else {
-
-		}
+		if (action == 'w') {							// handler for check action for input chars
+			charPos = charPos - 11;						
+														
+		}												
+		else if (action == 's') {						
+			charPos = charPos + 11;						
+		}												
+		else if (action == 'a') {						
+			charPos = charPos - 1;						
+		}												
+		else if (action == 'd') {						
+			charPos = charPos + 1;						
+		}												
+		else {											
+			std::cout << arrMessages[0];
+		}												
 		arrGameField[prevPos] = ' ';
 
 
